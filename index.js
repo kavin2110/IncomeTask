bd0689d99aa13281c554ad655bff8c5b6ef1aa48
kavin2110 <kavinb002@gmail.com>
2024-11-17 22:16:45 +0530

    Changes for popup message

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 // Reset form after submission
                 addForm.reset();
-                alert('Data added successfully');
+                alert('Data added successfully.Please refresh the screen once');
             } catch (error) {
                 console.error('Failed to add todo', error);
             }
